05f0af0f9cc7e7f1450422e10014ca3819984221
Jordan Judt <dev@jordanjudt.com>
2018-04-29 17:35:12 -0500

    Removed menu from TipCalc

--- app/src/main/java/com/machone/jcalc/view/tipcalc/TipCalcActivity.java
+++ app/src/main/java/com/machone/jcalc/view/tipcalc/TipCalcActivity.java
@@ -1,19 +1,14 @@
 package com.machone.jcalc.view.tipcalc;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.machone.jcalc.R;
-import com.machone.jcalc.view.AboutActivity;
 import com.machone.jcalc.view.extension.NonSwipeableViewPager;
 
 public class TipCalcActivity extends AppCompatActivity implements TipInputFragment.OnTipInputListener, TipOutputFragment.OnTipOutputListener {
@@ -38,47 +33,6 @@ public class TipCalcActivity extends AppCompatActivity implements TipInputFragme
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        MenuItem jcalc = menu.findItem(R.id.menuitem_jcalc);
-        jcalc.setEnabled(true);
-        jcalc.setVisible(true);
-
-        MenuItem tipcalc = menu.findItem(R.id.menuitem_tipcalc);
-        tipcalc.setEnabled(false);
-        tipcalc.setVisible(false);
-
-        MenuItem about = menu.findItem(R.id.menuitem_about);
-        about.setEnabled(true);
-        about.setVisible(true);
-
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menuitem_jcalc:
-                finish();
-                break;
-            case R.id.menuitem_about:
-                Intent intent = new Intent(getBaseContext(), AboutActivity.class);
-                startActivity(intent);
-                break;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-
-        return true;
-    }
-
-    @Override
     public void onSubtotalChanged(String formattedSubtotal) {
         subtotalTextView.setText("$" + formattedSubtotal);
     }
@@ -86,7 +40,7 @@ public class TipCalcActivity extends AppCompatActivity implements TipInputFragme
     @Override
     public void onSubmitButtonPressed() {
         viewPager.setCurrentItem(TIP_OUTPUT_FRAGMENT);
-        ((TipOutputFragment)((FragmentStatePagerAdapter)viewPager.getAdapter()).getItem(1)).setTipOutput(subtotalTextView.getText().toString());
+        ((TipOutputFragment) ((FragmentStatePagerAdapter) viewPager.getAdapter()).getItem(1)).setTipOutput(subtotalTextView.getText().toString());
     }
 
     @Override
@@ -95,7 +49,7 @@ public class TipCalcActivity extends AppCompatActivity implements TipInputFragme
         resetDisplay();
     }
 
-    private void resetDisplay(){
+    private void resetDisplay() {
         subtotalTextView.setText(getResources().getString(R.string.tip_default_output));
     }
 
