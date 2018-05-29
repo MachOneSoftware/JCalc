2f4c5c0a278f1c60b0bb259adcedb5618ec95e9c
Jordan Judt <dev@jordanjudt.com>
2018-05-28 23:45:24 -0500

    Prepare version and what's new

--- app/src/main/java/com/machone/jcalc/helper/VersionMap.java
+++ app/src/main/java/com/machone/jcalc/helper/VersionMap.java
@@ -8,6 +8,7 @@ public class VersionMap {
     public static final String verCode_8 = "1.2.0";
     public static final String verCode_9 = "1.2.1";
     public static final String verCode_10 = "1.2.2";
+    public static final String verCode_11 = "1.3.0";
 
     public static String getVersionName(int versionCode) {
         switch (versionCode) {
@@ -19,6 +20,8 @@ public class VersionMap {
                 return verCode_9;
             case 10:
                 return verCode_10;
+            case 11:
+                return verCode_11;
             default:
                 throw new IllegalArgumentException("Unexpected version code: " + versionCode);
         }
