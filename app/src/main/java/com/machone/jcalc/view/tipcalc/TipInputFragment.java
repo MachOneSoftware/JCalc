fc6833c4edce312d942db9fe2725cb281b5e857d
Jordan Judt <dev@jordanjudt.com>
2019-03-15 00:55:58 -0500

    TipCalc Dynamic button height

--- app/src/main/java/com/machone/jcalc/view/tipcalc/TipInputFragment.java
+++ app/src/main/java/com/machone/jcalc/view/tipcalc/TipInputFragment.java
@@ -1,12 +1,20 @@
 package com.machone.jcalc.view.tipcalc;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Rect;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.Button;
+import android.widget.TableLayout;
 
 import com.machone.jcalc.R;
 
@@ -17,6 +25,8 @@ import com.machone.jcalc.R;
  * to handle interaction events.
  */
 public class TipInputFragment extends Fragment {
+    private final String TAG = "TipInputFragment";
+
     private OnTipInputListener listener;
     private String subtotal = "";
 
@@ -26,6 +36,14 @@ public class TipInputFragment extends Fragment {
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.fragment_tip_input, container, false);
         registerClickListeners(view);
+
+        view.post(new Runnable() {
+            @Override
+            public void run() {
+                setButtonHeight(view);
+            }
+        });
+
         return view;
     }
 
@@ -108,5 +126,77 @@ public class TipInputFragment extends Fragment {
         String cents = out.substring(out.length() - 2);
 
         return dollars + "." + cents;
+    }
+
+    private void setButtonHeight(View view) {
+        Log.d(TAG, "setButtonHeight");
+
+        final Activity activity = getActivity();
+
+        View input = activity.findViewById(R.id.textview_subtotal_input);
+        View subtotalText = activity.findViewById(R.id.textview_subtotal_string);
+        ViewGroup.MarginLayoutParams inputLayout = (ViewGroup.MarginLayoutParams) input.getLayoutParams();
+        ViewGroup.MarginLayoutParams subtotalTextLayout = (ViewGroup.MarginLayoutParams) subtotalText.getLayoutParams();
+
+        // Get View heights to subtract from screen height
+        final int BUTTON_ROWS = ((TableLayout) view.findViewById(R.id.table_buttons)).getChildCount();
+        final int BUTTON_MARGIN = getResources().getDimensionPixelOffset(R.dimen.tipCalcButtonVerticalMargin) * (BUTTON_ROWS + 2);
+        final int INPUT_HEIGHT = input.getHeight() + subtotalText.getHeight();
+        final int INPUT_MARGIN = inputLayout.topMargin + inputLayout.bottomMargin + subtotalTextLayout.topMargin + subtotalTextLayout.bottomMargin;
+        final int BANNER_AD_HEIGHT = getResources().getDimensionPixelOffset(R.dimen.banner_ad_height);
+
+        // ActionBar height
+        int actionbarHeight = 0;
+        int usableHeight;
+        TypedValue typedValue = new TypedValue();
+        if (activity.getTheme().resolveAttribute(R.attr.actionBarSize, typedValue, true))
+            actionbarHeight = TypedValue.complexToDimensionPixelSize(typedValue.data, getResources().getDisplayMetrics());
+
+        // Soft key height
+        // getRealMetrics is only available with API 17+
+        int softKeyHeight = 0;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            DisplayMetrics metrics = new DisplayMetrics();
+            activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+            usableHeight = metrics.heightPixels;
+            activity.getWindowManager().getDefaultDisplay().getRealMetrics(metrics);
+            int realHeight = metrics.heightPixels;
+            if (realHeight > usableHeight)
+                softKeyHeight = realHeight - usableHeight;
+        } else {
+            // Get usable screen height
+            Rect displayRect = new Rect();
+            activity.getWindow().getDecorView().getWindowVisibleDisplayFrame(displayRect);
+            usableHeight = displayRect.height();
+        }
+
+        // Get status bar height to add
+        int statusBarHeight = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0)
+            statusBarHeight = getResources().getDimensionPixelSize(resourceId);
+
+        // Calculate usable space for buttons
+        int usable = usableHeight - actionbarHeight - INPUT_HEIGHT - INPUT_MARGIN - BANNER_AD_HEIGHT - BUTTON_MARGIN - softKeyHeight + statusBarHeight;
+        int buttonHeight = usable / BUTTON_ROWS;
+
+        // Get and set LayoutParams
+        Button zero = view.findViewById(R.id.zero);
+        ViewGroup.LayoutParams params = zero.getLayoutParams();
+        params.height = buttonHeight;
+
+        // Set LayoutParams for each button
+        zero.setLayoutParams(params);
+        view.findViewById(R.id.one).setLayoutParams(params);
+        view.findViewById(R.id.two).setLayoutParams(params);
+        view.findViewById(R.id.three).setLayoutParams(params);
+        view.findViewById(R.id.four).setLayoutParams(params);
+        view.findViewById(R.id.five).setLayoutParams(params);
+        view.findViewById(R.id.six).setLayoutParams(params);
+        view.findViewById(R.id.seven).setLayoutParams(params);
+        view.findViewById(R.id.eight).setLayoutParams(params);
+        view.findViewById(R.id.nine).setLayoutParams(params);
+        view.findViewById(R.id.cancel).setLayoutParams(params);
+        view.findViewById(R.id.submit).setLayoutParams(params);
     }
 }
